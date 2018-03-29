db281849f31e3bf25cad977973bb879de2b15ec7
Alexander Kolesnikov <kolesnikoff@gmail.com>
2018-03-29 18:25:45 +0300

    Platform.sh configuration files.

---
+++ web/sites/default/settings.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @file
+ * Platform.sh example settings.php file for Drupal 8.
+ */
+
+// Install with the 'thunder' profile for this example.
+$settings['install_profile'] = 'thunder';
+
+/**
+ * Default Drupal 8 settings.
+ *
+ * These are already explained with detailed comments in Drupal's
+ * default.settings.php file.
+ *
+ * See https://api.drupal.org/api/drupal/sites!default!default.settings.php/8
+ */
+$databases = array();
+$config_directories = array();
+$settings['update_free_access'] = FALSE;
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+
+// This is defined inside the read-only "config" directory, deployed via Git.
+$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';
+
+// Default PHP settings.
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 100);
+ini_set('session.gc_maxlifetime', 200000);
+ini_set('session.cookie_lifetime', 2000000);
+ini_set('pcre.backtrack_limit', 200000);
+ini_set('pcre.recursion_limit', 200000);
+
+
+// Automatic Platform.sh settings.
+if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
+  include $app_root . '/' . $site_path . '/settings.platformsh.php';
+}
+// Local settings. These come last so that they can override anything.
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
